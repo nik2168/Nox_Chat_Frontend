b2d6714471a4b4bd9dfc3bbc81d6124cc26c8e5c
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-07-23 20:07:36 +0530

    login ui change

--- src/socket.jsx
+++ src/socket.jsx
@@ -9,7 +9,6 @@ const getSocket = () => useContext(SocketContext);
 const SocketProvider = ({ children }) => {
     
   const socket = useMemo(() => io.connect(server, { withCredentials: true}), []);
-
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
@@ -20,14 +19,17 @@ const SocketProvider = ({ children }) => {
 
 //   const [socket, setSocket] = useState(null)
     
-//  useEffect(() => {
-//    const socket = io.connect(server, { withCredentials: true });
-//    setSocket(socket)
-//       console.log(" Socket :", socket);
+// useEffect(() => {
+//     const socket = io("http://localhost:3333", { withCredentials: true });
 
-//    			// return () => socket.close();
-//  }, []);
+//     setSocket(socket);
 
+
+//     // return () => socket.close();
+ 
+// }, []);
+
+//  console.log("OUter Socket :", socket)
 
 //   return (
 //     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
